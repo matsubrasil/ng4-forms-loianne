b20080a737a57a8f45835b2c0f14a27265f6441e
matsubrasil <matsuboy@outlook.com>
2017-07-15 21:10:51 -0300

    setValue e patchValue

--- src/app/component/template-form/template-form.component.ts
+++ src/app/component/template-form/template-form.component.ts
@@ -34,20 +34,73 @@ export class TemplateFormComponent implements OnInit {
 
   }
 
-  consultaCEP( cep ) {
+  consultaCEP( cep , form ) {
     console.log( 'cep', cep );
     cep = cep.replace(/\D/g, '');
     console.log( 'cep', cep );
     if (cep !== '') {
       const validacep = /^[0-9]{8}$/;
       if ( validacep.test( cep ) ) {
+
+        this.resetaFormulario( form );
+
         this.http.get( `//viacep.com.br/ws/${cep}/json/` )
           .map( dados => dados.json() )
-          .subscribe( (endereco) => {
-            console.log( 'endereco', endereco );
+          .subscribe( (dados) => {
+            // console.log( 'endereco', dados );
+            this.populaDadosFormulario( dados, form );
           });
+      } else {
+        // cep é inválido.
+        this.resetaFormulario( form );
+        alert('Formato de CEP inválido.');
       }
     }
+  }
+
+  populaDadosFormulario( dados , formulario ) {
+
+    /*
+      formulario.setValue({
+          nome: formulario.value.nome,
+          email: formulario.value.email,
+          endereco: {
+            rua: dados.logradouro,
+            cep: dados.cep,
+            numero: '' ,
+            complemento: dados.complemento ,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf
+          }
+        }
+      );
+    */
+
+    formulario.form.patchValue({
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaFormulario( formulario ) {
+    formulario.form.patchValue({
+      endereco: {
+          cep: null,
+          rua: null,
+          numero: null,
+          complemento: null,
+          bairro: null,
+          cidade: null,
+          estado: null
+        }
+    });
   }
   onSubmit(form) {
     console.log( 'submit', form );
