23e88d46255526f68ee12ca553b4bc62218f1dd4
matsubrasil <matsuboy@outlook.com>
2017-07-17 01:37:57 -0300

    reajustando modulo shared

--- src/app/component/data-form/data-form.component.ts
+++ src/app/component/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -7,10 +7,22 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./data-form.component.css']
 })
 export class DataFormComponent implements OnInit {
+
   formulario: FormGroup;
-  constructor() { }
+
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit() {
+
+    // this.formulario = new FormGroup({
+    //   nome: new FormControl( null ),
+    //   email: new FormControl( null )
+    // });
+
+    this.formulario = this.formBuilder.group({
+      nome: [ null ],
+      email: [ null ]
+    });
   }
 
 }
--- src/app/component/data-form/data-form.module.ts
+++ src/app/component/data-form/data-form.module.ts
@@ -1,13 +1,19 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from './../shared/shared.module';
+
 import { DataFormComponent } from './data-form.component';
+
+
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
   declarations: [
     DataFormComponent
